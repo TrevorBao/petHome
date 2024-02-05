7848169fb3510ca441d6493e7f20a8b0e1b3a503
Renhao Bao <815177147@qq.com>
2024-02-05 19:52:19 +0000

    Firebase setup and trial

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,180 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Auth from "./components/Auth";
+import { db, auth, storage } from "./firebase";
+import {
+  collection,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  doc,
+  onSnapshot,
+} from "firebase/firestore";
+import { ref, uploadBytes } from "firebase/storage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  type Pet = {
+    id: string;
+    [key: string]: string;
+  };
+
+  const [petList, setPetList] = useState<Pet[]>([]);
+
+  // Create new Pet Info
+  const [petName, setPetName] = useState("");
+  const [petType, setPetType] = useState("");
+  const [petBreed, setPetBreed] = useState("");
+
+  // Update Pet Info
+  const [updatedPetName, setUpdatedPetName] = useState("");
+
+  // File Upload State
+  const [fileUpload, setFileUpload] = useState<FileList | null>(null);
+
+  const petCollectionRef = collection(db, "petInfo");
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(
+      petCollectionRef,
+      (snapshot) => {
+        const pets: Pet[] = snapshot.docs.map((doc) => ({
+          ...(doc.data() as Pet),
+          id: doc.id,
+        }));
+        setPetList(pets);
+      },
+      (err) => {
+        console.error(err);
+      }
+    );
+
+    return () => unsubscribe();
+  }, []);
+
+  const onSubmitPet = async () => {
+    try {
+      await addDoc(petCollectionRef, {
+        breed: petBreed,
+        type: petType,
+        name: petName,
+        userId: auth?.currentUser?.uid,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const deletePet = async (id: string) => {
+    if (!auth.currentUser) {
+      console.error("No authenticated user.");
+      return;
+    }
+
+    const petDocRef = doc(db, "petInfo", id);
+    const petDocSnap = await getDoc(petDocRef);
+
+    if (!petDocSnap.exists()) {
+      console.error("Document does not exist.");
+      return;
+    }
+
+    const petData = petDocSnap.data();
+    if (petData.userId !== auth.currentUser.uid) {
+      console.error(
+        "Authenticated user does not match the userId of the document."
+      );
+      return;
+    }
+
+    try {
+      await deleteDoc(petDocRef);
+      console.log("Document successfully deleted.");
+    } catch (err) {
+      console.error("Delete error:", err);
+    }
+  };
+
+  const updatePetName = async (id: string) => {
+    try {
+      const petDoc = doc(db, "petInfo", id);
+      await updateDoc(petDoc, { name: updatedPetName });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const uploadFiles = async () => {
+    if (fileUpload) {
+      for (let i = 0; i < fileUpload.length; i++) {
+        const file = fileUpload.item(i);
+        if (file) {
+          const fileFolderRef = ref(storage, `users/${file.name}`);
+          try {
+            await uploadBytes(fileFolderRef, file);
+          } catch (err) {
+            console.error(err);
+          }
+        }
+      }
+    }
+  };
 
   return (
     <>
+      <Auth />
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input
+          type="text"
+          placeholder="Name of the Pet"
+          onChange={(e) => setPetName(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Type of the Pet"
+          onChange={(e) => setPetType(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Breed of the Pet"
+          onChange={(e) => setPetBreed(e.target.value)}
+        />
+        <button onClick={onSubmitPet}>Submit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div>
+        {petList.map((pet) => (
+          <div key={pet.id}>
+            <h1>{pet.name}</h1>
+            <p>{pet.type}</p>
+            <p>{pet.breed}</p>
+
+            <button onClick={() => deletePet(pet.id)}>Delete</button>
+
+            <input
+              type="text"
+              placeholder={pet.name}
+              onChange={(e) => setUpdatedPetName(e.target.value)}
+            />
+            <button onClick={() => updatePetName(pet.id)}>Update</button>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        <input
+          type="file"
+          multiple
+          onChange={(e) => {
+            if (e.target.files && e.target.files.length > 0) {
+              setFileUpload(e.target.files);
+            }
+          }}
+        />
+        <button onClick={uploadFiles}>Upload File</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsvHOT2kBBimiW8oAdCIMh5Z5ArJPihQ8",
+  authDomain: "pet-home-1c0a2.firebaseapp.com",
+  databaseURL: "https://pet-home-1c0a2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "pet-home-1c0a2",
+  storageBucket: "pet-home-1c0a2.appspot.com",
+  messagingSenderId: "673768431341",
+  appId: "1:673768431341:web:3a171124cb204300e3f6e4",
+  measurementId: "G-71ND2D38B0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const googleprovider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
