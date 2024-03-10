953448bde165c32986cc3871b189235da8b63e43
Renhao Bao <815177147@qq.com>
2024-03-10 22:48:17 +0000

    add rehomed pets count card

---
+++ src/components/RehomedCounts.tsx
@@ -0,0 +1,77 @@
+import {
+  Card,
+  CardBody,
+  Text,
+  Flex,
+  Box,
+  Tooltip,
+  Icon,
+} from "@chakra-ui/react";
+import DataBlueIcon from "../assets/dataBlue.svg";
+import usePetsByUserId from "../hooks/usePetsByUserId";
+import { QuestionIcon } from "@chakra-ui/icons";
+
+interface Props {
+  userId: string;
+}
+
+const RehomedCounts = ({ userId }: Props) => {
+  const { petsCount } = usePetsByUserId({ userId });
+
+  return (
+    <Card
+      borderRadius="3xl"
+      shadow="lg"
+      overflow="hidden"
+      variant="elevated"
+      maxW={{ base: "80vw", md: "fit-content" }}
+      p={1}
+      pl={4}
+      pb={0}
+      display="flex"
+      justifyContent="center"
+    >
+      <CardBody>
+        <Flex alignItems="center" justifyContent="center" wrap="wrap">
+          <Box>
+            <Text fontSize="3xl" fontWeight={500} color="#90c1dd">
+              {petsCount.rehomedCount}
+            </Text>
+            <Text py="2">Rehomed Pets</Text>
+          </Box>
+          <Box
+            flexShrink={0}
+            position="relative"
+            maxW={{ base: "200px", sm: "220px" }}
+            height="auto"
+          >
+            <Tooltip
+              hasArrow
+              label="The number of your rehoming pets that has been successfully adopted"
+              bg="#e6e6e6"
+              color="black"
+            >
+              <Icon
+                as={QuestionIcon}
+                color="#e3e3e3"
+                position="absolute"
+                right="6"
+                top="0"
+              />
+            </Tooltip>
+            <img
+              src={DataBlueIcon}
+              alt="Decoration Image"
+              style={{
+                width: "100%",
+                height: "auto",
+              }}
+            />
+          </Box>
+        </Flex>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default RehomedCounts;
--- src/routing/UserProfilePage.tsx
+++ src/routing/UserProfilePage.tsx
@@ -6,6 +6,7 @@ import { useParams } from "react-router-dom";
 import RehomedPetsCard from "../components/RehomedPetsCard";
 import AdoptedPetsCard from "../components/AdoptedPetsCard";
 import RehomingCounts from "../components/RehomingCounts";
+import RehomedCounts from "../components/RehomedCounts";
 
 const UserProfilePage = () => {
   const { ownerUser } = useUsers();
@@ -17,6 +18,7 @@ const UserProfilePage = () => {
         <Heading mb={7}>Profile</Heading>
         {ownerUser && <ProfileCard user={ownerUser} />}
         {id && <RehomingCounts userId={id} />}
+        {id && <RehomedCounts userId={id} />}
         {id && <AdoptedPetsCard userId={id} />}
         {id && <RehomingPetsCard userId={id} />}
         {id && <RehomedPetsCard userId={id} />}
